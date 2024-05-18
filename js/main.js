6b3f166ff561d280d292122564ccc7a73a286637
GuntherCoad <gunthercoad@yahoo.com>
2024-05-18 07:48:29 -0500

    added basic password generation to page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 const lowerAlpha="abcdefghijklmnopqrstuvwxyz";
 const upperAlpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers="0123456789";
-const extraSymbols=".!@#$%^&*()/"
+const extraSymbols="\.\!\@\#\$\%\^\&\*\(\)\/\-\_";
 const buttonUser = document.getElementById("generateButton");
 
 
-function generatePassword() {
-    s
+function generatePassword(passSize) {
+    randArr = [lowerAlpha, extraSymbols, numbers, upperAlpha];
+    password='';
+    randArrNum = 0;
+    for (let index = 0; index < passSize; index++) {
+        randArrNum = Math.round(Math.random() * (randArr.length - 1));
+        password += getRandomChar(randArr[randArrNum]);
+    }
+    //password.join("")
+    console.log(password);
 }
 
 //randomly picks a element from one of the strings
+//probably need to sanitize extraSymbols
 function getRandomChar(stringA) {
-    randNum = Math.round(Math.random() * stringA.length);
-    console.log(stringA[randNum]);
+    randNum = Math.round(Math.random() * (stringA.length - 1) );
+    
+    return stringA[randNum];
 }
 
 
 
 buttonUser.addEventListener("click", function() {
-    getRandomChar(lowerAlpha);
+    generatePassword(20);
 } );
